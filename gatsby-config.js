d585114f31ae9af52437e959d7f743a87ada7d44
Cees <cjol99@icloud.com>
2020-11-26 12:25:43 +0100

    Try to fix GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,18 +10,6 @@ module.exports = {
       options: {
         // The property ID; the tracking code won't be generated without it
         trackingId: "G-ZRZSD16RP5",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: [],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Defers execution of google analytics script after page load
-        defer: false,
       },
     },
     `gatsby-plugin-react-helmet`,
